002f8e0e5a87597cb29921b5a5425fee24eb17bb
Zhongtian Dong <78445813+ZhongtianD@users.noreply.github.com>
2021-03-13 20:46:55 -0600

    Add files via upload

---
+++ Analyze_samples.py
@@ -0,0 +1,168 @@
+# imports of external packages to use in our code
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import our Gaussian2 class from Random.py file
+sys.path.append(".")
+from Random import Gaussian2
+
+# main function for our analysis code
+if __name__ == "__main__":
+    
+    #sigma is the standard deviation for the gaussian distribution of the sample
+    #default sigma value for hypothesis 0
+    sigma_0 = 1
+    #default sigma value for hypothesis 1
+    sigma_1 = 1
+    
+    #mu0 is the mean for the gaussian distribution of the mean
+    # default mu0 value for hypothesis 0
+    mu0_0 = 0
+    # default mu0 value for hypothesis 1
+    mu0_1 = 1
+    
+    #sigma0 is the standard deviation for the gaussian distribution of the mean
+    #default sigma0 value for hypothesis 0
+    sigma0_0 = 0.3
+    #default sigma0 value for hypothesis 1
+    sigma0_1 = 0.3
+    
+    # default alpha value
+    alpha = 0.05
+
+    haveH0 = False
+    haveH1 = False
+
+    if '-mu0_0' in sys.argv:
+        p = sys.argv.index('-mu0_0')
+        mu0_0 = float(sys.argv[p+1])
+    if '-mu0_1' in sys.argv:
+        p = sys.argv.index('-mu0_1')
+        mu0_1 = float(sys.argv[p+1])
+    if '-sigma0' in sys.argv:
+        p = sys.argv.index('-sigma0')
+        ptemp = float(sys.argv[p+1])
+        if ptemp > 0 :
+            sigma_0 = ptemp
+            sigma_1 = ptemp
+    if '-sigma1' in sys.argv:
+        p = sys.argv.index('-sigma1')
+        ptemp = float(sys.argv[p+1])
+        if ptemp > 0 :
+            sigma_1 = ptemp
+    if '-sigma0_0' in sys.argv:
+        p = sys.argv.index('-sigma0_0')
+        ptemp = float(sys.argv[p+1])
+        if ptemp > 0 :
+            sigma0_0 = ptemp
+            sigma0_1 = ptemp
+    if '-sigma0_1' in sys.argv:
+        p = sys.argv.index('-sigma0_1')
+        ptemp = float(sys.argv[p+1])
+        if ptemp > 0 :
+            sigma0_1 = ptemp
+    if '-alpha' in sys.argv:
+        p = sys.argv.index('-alpha')
+        ptemp = float(sys.argv[p+1])
+        if (ptemp > 0 and ptemp<1) :
+            alpha = ptemp
+    if '-input0' in sys.argv:
+        p = sys.argv.index('-input0')
+        InputFile0 = sys.argv[p+1]
+        haveH0 = True
+    if '-input1' in sys.argv:
+        p = sys.argv.index('-input1')
+        InputFile1 = sys.argv[p+1]
+        haveH1 = True
+    if '-h' in sys.argv or '--help' in sys.argv or not haveH1:
+        print ("Usage: %s [options]" % sys.argv[0])
+        print ("  options:")
+        print ("   --help(-h)          print options")
+        print ("   -input0 [filename]  name of file for H0 data")
+        print ("   -input1 [filename]  name of file for H1 data")
+        print ("   -mu0_0 [number]     mu0 for H0")
+        print ("   -mu0_1 [number]     mu0 for H1")
+        print ("   -sigma0 [number]   sigma for H0 ( and H1 if there is no sigma1 input)")
+        print ("   -sigma1 [number]   sigma for H1")
+        print ("   -sigma0_0 [number]   sigma0 for H0 ( and H1 if there is no sigma1 input)")
+        print ("   -sigma0_1 [number]   sigma0 for H1")
+        print ("   -alpha [number]   alpha value for H0")
+        print
+        sys.exit(1)
+    
+    Nsample = 1
+    LogLikeRatio0 = []
+    LogLikeRatio1 = []
+    #initialise Gaussian models
+    G_0 = Gaussian2(sigma = sigma_0, mu0 = mu0_0 ,sigma0 = sigma0_0)
+    G_1 = Gaussian2(sigma = sigma_1, mu0 = mu0_1 ,sigma0 = sigma0_1)
+
+    LLR_min = 1e8
+    LLR_max = -1e8
+    
+    #loading files    
+    file0 = np.load(InputFile0)
+    Nsample = file0.shape[1]
+        
+    for i in range(file0.shape[0]):
+        Nsum = 0
+        LLR = 0
+        for v in file0[i]:
+            #loglikelihood ratio
+            LLR += G_1.loglike(v)-G_0.loglike(v)
+
+                
+                    
+
+        if LLR < LLR_min:
+              LLR_min = LLR
+        if LLR > LLR_max:
+              LLR_max = LLR
+        LogLikeRatio0.append(LLR)
+
+    if haveH1:
+        file1 = np.load(InputFile1)
+            
+        for i in range(file1.shape[0]):
+            LLR = 0
+            for v in file1[i]:
+                #loglikelihood ratio
+                LLR += G_1.loglike(v)-G_0.loglike(v)
+
+                
+                    
+
+            if LLR < LLR_min:
+                LLR_min = LLR
+            if LLR > LLR_max:
+                LLR_max = LLR
+            LogLikeRatio1.append(LLR)
+
+    title = str(Nsample) +  " samples / experiment"
+    #sort sequence to find $\\lambda_\\alpha$ and beta
+    LogLikeRatio0.sort()
+    LogLikeRatio1.sort()
+    L_alpha = LogLikeRatio0[int(len(LogLikeRatio0)*(1-alpha))]
+    res = next(i for i,v in enumerate(LogLikeRatio1) if v > L_alpha)
+    beta = res/len(LogLikeRatio1)
+    
+
+    # make LLR figure
+    plt.figure()
+    plt.hist(LogLikeRatio0, 50, density=True, facecolor='b', alpha=0.5, label="assuming $\\mathbb{H}_0$")
+    plt.hist(LogLikeRatio1, 50, density=True, facecolor='g', alpha=0.5, label="assuming $\\mathbb{H}_1$")
+    plt.axvline(alpha, color='r', linewidth=1, label='$\\lambda_\\alpha$')
+    plt.plot([], [], ' ', label="$\\alpha = $ alpha")
+    plt.plot([], [], ' ', label="$\\beta = $"+str(beta) ) 
+    plt.legend()
+
+    plt.xlabel('$\\lambda = \\log({\\cal L}_{\\mathbb{H}_{1}}/{\\cal L}_{\\mathbb{H}_{0}})$')
+    plt.ylabel('Probability')
+    plt.title(title)
+
+    plt.grid(True)
+
+    plt.savefig('Gaussian with varying mean.png')
+    plt.show()
+    
---
+++ Take_samples.py
@@ -0,0 +1,89 @@
+import sys
+import numpy as np
+
+
+sys.path.append(".")
+from Random import Gaussian2
+
+#main function 
+if __name__ == "__main__":
+
+    # default seed
+    seed = 2048
+    
+    # default deviation of the distribution of the sample
+    sigma = 1
+
+    # default mean of the distribution of mean
+    mu0 = 0
+    
+    # default deviation of the distribution of mean
+    sigma0 = 0.3
+    
+
+    # default number of samples (per experiment)
+    Nsample = 10
+
+    # default number of experiments
+    Nexp = 1
+
+    # output file defaults
+    doOutputFile = False
+
+    if '-h' in sys.argv or '--help' in sys.argv:
+        print ("Usage: %s [options]" % sys.argv[0])
+        print ("  options:")
+        print ("   --help(-h)          print options")
+        print ("   -seed [number] seed for random number generation")
+        print ("   -sigma [number] standard deviation for the gaussian distribution of the sample")
+        print ("   -mu0 [number] mean for the gaussian distribution of the mean")
+        print ("   -sigma0 [number] standard deviation for the gaussian distribution of the mean")
+        print ("   -Nsample [number] number of samples for each experiment")
+        print ("   -Nexp [number] number of experiments")
+        print ("   -output [.npy file] output file saved in the .npy format")
+        print
+        sys.exit(1)
+    
+    if '-seed' in sys.argv:
+        p = sys.argv.index('-seed')
+        seed = sys.argv[p+1]
+    if '-mu0' in sys.argv:
+        p = sys.argv.index('-mu0')
+        mu0 = float(sys.argv[p+1])
+    if '-sigma' in sys.argv:
+        p = sys.argv.index('-sigma')
+        ptemp = float(sys.argv[p+1])
+        if ptemp > 0 :
+            sigma = ptemp
+    if '-sigma0' in sys.argv:
+        p = sys.argv.index('-sigma0')
+        ptemp = float(sys.argv[p+1])
+        if ptemp > 0 :
+            sigma = ptemp
+    if '-Nsample' in sys.argv:
+        p = sys.argv.index('-Nsample')
+        Ns = int(sys.argv[p+1])
+        if Ns > 0:
+            Nsample = Ns
+    if '-Nexp' in sys.argv:
+        p = sys.argv.index('-Nexp')
+        Ne = int(sys.argv[p+1])
+        if Ne > 0:
+            Nexp = Ne
+    if '-output' in sys.argv:
+        p = sys.argv.index('-output')
+        OutputFileName = sys.argv[p+1]
+        doOutputFile = True
+
+    # Initialze an object of the Gaussian class
+    G = Gaussian2(seed = seed, sigma = sigma, mu0 = mu0 ,sigma0 = sigma0)
+
+    if doOutputFile:
+        S = np.zeros((Nexp,Nsample))
+        #Generate samples
+        for e in range(Nexp):
+            x = np.random.rand()
+            #Generate samples with a random starting point
+            S[e] = G.Gaussian_sample([mu0, mu0+x*sigma ],Nsample)
+        np.save(OutputFileName, S)
+   
